f3bda5f072b9e3341113d6c5837ce2b7c3477232
meunpredictable <vishalpatel654@gmail.com>
2023-09-12 16:29:09 +1000

    12 sep

--- Self_Attention.py
+++ Self_Attention.py
@@ -1,4 +1,4 @@
-# base code sourced from :https://datascience.stackexchange.com/questions/94205/a-simple-attention-based-text-prediction-model-from-scratch-using-pytorch
+# Inspired from :https://datascience.stackexchange.com/questions/94205/a-simple-attention-based-text-prediction-model-from-scratch-using-pytorch
 
 # authur: me__unpredictable
 import torch
@@ -7,27 +7,33 @@ import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 random.seed(0)
 torch.manual_seed(0)
 
 # Sample text for Training
-test_sentence = """Thomas Edison. The famed American inventor rose to prominence in the late
-19th century because of his successes, yes, but even he felt that these successes
-were the result of his many failures. He did not succeed in his work on one of his
-most famous inventions, the lightbulb, on his first try nor even on his hundred and
-first try. In fact, it took him more than 1,000 attempts to make the first incandescent
-bulb but, along the way, he learned quite a deal. As he himself said,
-"I did not fail a thousand times but instead succeeded in finding a thousand ways it would not work." 
-Thus Edison demonstrated both in thought and action how instructive mistakes can be. 
-""".lower().split()
+test_sentence = """The sayings of King Lemuel—an inspired utterance his mother taught him. Listen, my son! Listen, son of my womb! Listen, my son, the answer to my prayers! Do not spend your strength[a] on women, your vigor on those who ruin kings. It is not for kings, Lemuel—
+    it is not for kings to drink wine, not for rulers to crave beer, lest they drink and forget what has been decreed, and deprive all the oppressed of their rights. Let beer be for those who are perishing,
+    wine for those who are in anguish! Let them drink and forget their poverty and remember their misery no more. Speak up for those who cannot speak for themselves,
+    for the rights of all who are destitute. Speak up and judge fairly; defend the rights of the poor and needy. A wife of noble character who can find?
+    She is worth far more than rubies. Her husband has full confidence in her and lacks nothing of value. She brings him good, not harm,
+    all the days of her life. She selects wool and flax and works with eager hands. She is like the merchant ships,
+    bringing her food from afar. She gets up while it is still night; she provides food for her family and portions for her female servants. She considers a field and buys it;  out of her earnings she plants a vineyard. She sets about her work vigorously; her arms are strong for her tasks. She sees that her trading is profitable,
+    and her lamp does not go out at night. In her hand she holds the distaff and grasps the spindle with her fingers. She opens her arms to the poor
+    and extends her hands to the needy. When it snows, she has no fear for her household; for all of them are clothed in scarlet. She makes coverings for her bed;
+    she is clothed in fine linen and purple. Her husband is respected at the city gate, where he takes his seat among the elders of the land. She makes linen garments and sells them,
+    and supplies the merchants with sashes. She is clothed with strength and dignity; she can laugh at the days to come. She speaks with wisdom,
+    and faithful instruction is on her tongue. She watches over the affairs of her household and does not eat the bread of idleness. Her children arise and call her blessed;
+    her husband also, and he praises her: “Many women do noble things, but you surpass them all.” Charm is deceptive, and beauty is fleeting;
+    but a woman who fears the Lord is to be praised. Honor her for all that her hands have done, and let her works bring her praise at the city gate.""".lower().split()
 
 # Build a list of tuples.  Each tuple is ([ word_i-2, word_i-1 ], target word)
 trigrams = [([test_sentence[i], test_sentence[i + 1]], [test_sentence[i + 2],test_sentence[i + 3],test_sentence[i + 4],test_sentence[i + 5]])
             for i in range(len(test_sentence) - 5)]
 
 # print the first 3, just so you can see what they look like
-print(trigrams[:3])
+# print(trigrams[:3])
 
 vocab = list(set(test_sentence))
 word_to_ix2 = {word: i for i, word in enumerate(vocab)}
@@ -37,7 +43,7 @@ word_to_ix2 = {word: i for i, word in enumerate(vocab)}
 OUTPUT_SIZE=4
 
 # Number of Epochs
-EPOCHS = 25
+EPOCHS = 10
 
 # SEQ_SIZE is the number of words we are using as a context for the next word we want to predict
 SEQ_SIZE = 4
@@ -98,8 +104,9 @@ model.to('cuda')
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # Training
+loss_=[]
 for i in range(EPOCHS):
-    total_loss = 0
+    total_loss = []
     for context, target in trigrams:
         # context, target = ['thomas', 'edison.'] the
 
@@ -123,10 +130,18 @@ for i in range(EPOCHS):
         # step 6: updating the weights
         optimizer.step()
 
-        total_loss += loss.item()
-    if i % 2 == 0:
-        print("Epoch: ", str(i), " Loss: ", str(total_loss))
-
+        total_loss.append(loss.item())
+    total_loss=np.mean(total_loss)
+    print('\r',end=' ')
+    print("Epoch: ", str(i), " Loss: ", str(total_loss),end=' ')
+    loss_.append(total_loss)
+plt.plot(loss_)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Epoch vs Loss')
+plt.legend(['Loss'])
+plt.grid()
+plt.show()
 # Prediction
 with torch.no_grad():
     # Fetching a random context and target
